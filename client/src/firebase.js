ce1fd56fceb6e0ab566817f19ea242f3034f4253
Vinay <vinayvashishth43@gmail.com>
2023-10-14 10:22:03 +0530

    integrate the Google Firebase (OAuth Functionality)

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCyB11ifX7SfTeWd3s4rcVw6-NkS1ugxE",
+  authDomain: "real-estate-8e5a4.firebaseapp.com",
+  projectId: "real-estate-8e5a4",
+  storageBucket: "real-estate-8e5a4.appspot.com",
+  messagingSenderId: "434582984524",
+  appId: "1:434582984524:web:a468769414bfc7b69661e9"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import {Link, useNavigate} from "react-router-dom"
 import {useState} from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice"
+import OAuth from "../components/OAuth"
 
 function SignIn() {
 
@@ -31,7 +32,7 @@ function SignIn() {
         return
       }
       console.log(data)
-      dispatch(signInSuccess(data))
+      dispatch(signInSuccess(data.user))
       navigate('/')
     } catch(error){
       dispatch(signInFailure(error.message))
@@ -47,6 +48,8 @@ function SignIn() {
         <input onChange={handleChange} type="email" placeholder="email" className="border p-3 rounded-lg" id="email" />
         <input onChange={handleChange} type="password" placeholder="password" className="border p-3 rounded-lg" id="password" />
         <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-60">{loading?"Loading...":"Sign In"}</button>
+      
+        <OAuth />
       </form>
       <div className="flex gap-2 mt-5">
         <p> Dont have an account ?</p>
