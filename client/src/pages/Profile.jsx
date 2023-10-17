f789db3b36a4ee1ce70dde815bd212d4ed305040
Vinay <vinayvashishth43@gmail.com>
2023-10-18 00:25:51 +0530

    complete showUserListings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,6 +13,8 @@ function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false)
   const [formData, setFormData] = useState({})
   const [updateSuccess, setUpdateSuccess] = useState(false)
+  const [showListingsError, setShowListingsError] = useState(false)
+  const [userListings, setUserListings] = useState([])
   const dispatch = useDispatch()
 
   useEffect(()=>{
@@ -111,6 +113,21 @@ function Profile() {
     }
   }
 
+  const handleShowListings = async() => {
+    try{
+      setShowListingsError(false)
+      const res = await fetch(`/api/user/listings/${currentUser._id}`)
+      const data = await res.json()
+      if(data.success===false){
+        setShowListingsError(true)
+        return
+      }
+      setUserListings(data)
+    } catch(error){
+      setShowListingsError(true)
+    }
+  }
+
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -151,6 +168,38 @@ function Profile() {
 
       <p className="text-red-700 mt-5">{error? error : " "} </p>
       {updateSuccess && <p className="text-green-500">User updated successfully</p>}
+
+
+      <button className="text-green-700 w-full" onClick={handleShowListings} > Show Listings </button>
+      {showListingsError && <p className="text-red-700 mt-5">Error in showing listings</p>}
+      
+
+      
+      {userListings.length > 0 && 
+      <div className="flex flex-col gap-4">
+        <h1 className="text-center my-7 text-2xl font-semibold">Your Listings</h1>
+        {userListings.map((listing) => (
+          <div key={listing._id} className="border rounded-lg p-3 flex justify-between items-center gap-4">
+            <Link to={`/listing/${listing._id}`}>
+              <img src={listing.imageUrls[0]} alt="listing cover" className="h-16 w-16 object-contain rounded-lg" />
+            </Link>
+            <Link className="flex-1 text-slate-700 font-semibold hover:underline truncate" to={`/listing/${listing._id}`}>
+              <p>{listing.name}</p>
+            </Link>
+            <div className="flex flex-col items-center">
+              <button className="text-red-700 uppercase">
+                Delete
+              </button>
+              <button className="text-green-700 uppercase">
+                Edit
+              </button>
+            </div>
+
+          </div>
+        ))}
+      </div>
+      }
+
     </div>
   )
 }
