2b6386ff9dfb43758f7cc45769b58b63686fe97c
Vinay <vinayvashishth43@gmail.com>
2023-10-28 15:57:13 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,12 +5,15 @@ import userRouter from "./routes/user.js"
 import authRouter from "./routes/auth.js"
 import listingRouter from "./routes/listing.js"
 import cookieParser from "cookie-parser"
+import path from 'path'
 dotenv.config()
 
 
 mongoose.connect(process.env.DB_URL)
 .then(()=>(console.log("DB Connected Successfully")))
-// .catch(console.log("DB Connection Failed"))
+.catch(()=>console.log("DB Connection Failed"))
+
+const __dirname = path.resolve()
 
 const app = express()
 
@@ -33,6 +36,12 @@ app.get('/test', (req,res)=>{
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 app.use("/api/listing", listingRouter)
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 // This para shows the better ways of error handling 
 app.use((err, req, res, next) => {
