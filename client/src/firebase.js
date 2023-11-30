c7330bd06f201007695c4b62a52585f879afcf22
Vinay <vinayvashishth43@gmail.com>
2023-12-01 03:29:12 +0530

    corrections

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: "AIzaSyCCyB11ifX7SfTeWd3s4rcVw6-NkS1ugxE",
   authDomain: "real-estate-8e5a4.firebaseapp.com",
   projectId: "real-estate-8e5a4",
   storageBucket: "real-estate-8e5a4.appspot.com",
